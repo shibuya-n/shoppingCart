f2176fd9636b6e170789ffd71bd1040e4156c5b1
shibuya-n <109704872+shibuya-n@users.noreply.github.com>
2023-12-09 19:30:38 -0800

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // combine items if they're the same +4
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,8 +10,19 @@ public class Main {
     static Item[] shoppingCart = new Item[365];
     static int count = 0;
     public static void main(String[] args) {
+
+
         boolean done = false;
         while (!done) {
+            String[] cartContents = new String[count];
+
+            System.out.println("Current Shopping Cart:");
+            for(int i = 0; i < count; i++){
+
+                cartContents[i] = shoppingCart[i].getName();
+            }
+            System.out.println(Arrays.toString(cartContents));
+
             System.out.println("What do you want to do?");
             System.out.println("1. Add items");
             System.out.println("2. Remove items");
@@ -26,17 +38,20 @@ public class Main {
                 removeItems();
             }
             else if (response.equals("3")){
-                System.out.println("Total = $" +itemTotal());
+
+
+                System.out.println("Total = $" + itemTotal());
             }
 
             else {
                 done = true;
+                System.out.println("Thank you for shopping with us!");
             }
         }
     }
 
 
-    public static void addItems(){
+    public static void addItems() {
         System.out.println("What item do you want to add?");
         String itemName = getInput();
         System.out.println("How many of that item are you adding?");
@@ -44,14 +59,28 @@ public class Main {
         System.out.println("How much does this item cost?");
         double itemCost = Double.parseDouble(getInput());
 
+        for (int i = 0; i < count; i++) {
+            Item x = shoppingCart[i];
+            if ((itemName.equals(x.getName())) && (itemCost == x.getPrice())) {
+                shoppingCart[i].quantity += itemCount;
+                break;
+            }
+            else {
+                shoppingCart[count] = new Item(itemName, itemCost, itemCount);
+                count++;
 
-        shoppingCart[count] = new Item(itemName, itemCost, itemCount);
+            }
+
+
+        }
     }
 
     public static void removeItems(){
 
     }
     public static double itemTotal(){
+
+
         double total = 0;
         for (int i = 0; i < count; i++){
             Item x = shoppingCart[i];
